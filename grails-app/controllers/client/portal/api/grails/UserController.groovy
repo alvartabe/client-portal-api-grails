bee2d421162820d890d84210f5db8108a2179d8d
aartavia <aartavia@enerbankusa.com>
2023-08-04 15:23:10 -0600

    filter by

--- grails-app/controllers/client/portal/api/grails/UserController.groovy
+++ grails-app/controllers/client/portal/api/grails/UserController.groovy
@@ -11,10 +11,23 @@ class UserController  {
 
     @Secured("ROLE_ADMIN")
     def index() {
+        // get the loggedInUser
         def loggedInUser = springSecurityService.currentUser
+
+        // find all the users except loggedInUser
         def users = User.findAll {
             ne('id', loggedInUser.id)
         }
+
+        // Get the field to sort on from the params
+        String sortBy = params.sortBy ?: 'id'
+
+        // Dynamic sorting based on the provided sortBy
+        users.sort { it[sortBy] }
+
+        // Optionally reverse the list if sortOrder is 'desc'
+        users = params.sortOrder == 'desc' ? users.reverse() : users
+
         render users as JSON;
     }
 
